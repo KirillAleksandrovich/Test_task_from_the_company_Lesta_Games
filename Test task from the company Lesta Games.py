e9798c31ac738f1a4f6b28bea22e21882bc1a5d6
Kirill <Kirill.Feshev@bk.ru>
2025-03-29 19:07:02 +0300

    Создаю основное приложение Flask, папку для загрузки файлов и папку для загрузок.

--- Test task from the company Lesta Games.py
+++ Test task from the company Lesta Games.py
@@ -5,3 +5,12 @@ import re  # Для регулярных выражений (очистка те
 import math  # Для математических операций (логарифм для IDF)
 from collections import defaultdict  # Для удобного подсчёта частот слов
 
+# Создаём основное приложение Flask
+app = Flask(__name__)
+
+# Конфигурация: папка для загрузки файлов
+app.config['UPLOAD_FOLDER'] = 'zagruzki'
+
+# Создаём папку для загрузок, если её нет
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
